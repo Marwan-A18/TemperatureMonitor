d362baa44d60842643e797bbb0429776cfa912e4
Marwan Ahmed <marwan.ahmed.0611@gmail.com>
2025-05-13 16:37:27 +0300

    First Edit

--- Monitor/temp_monitor.c
+++ Monitor/temp_monitor.c
@@ -1,8 +1,7 @@
-/*
- * temp_monitor.c
- *
- *  Created on: May 13, 2025
- *      Author: Marwan Ahmed
- */
+
+
+
+
+
 
 
--- Monitor/temp_monitor.h
+++ Monitor/temp_monitor.h
@@ -1,10 +1,3 @@
-/*
- * temp_monitor.h
- *
- *  Created on: May 13, 2025
- *      Author: Marwan Ahmed
- */
-
 #ifndef MONITOR_TEMP_MONITOR_H_
 #define MONITOR_TEMP_MONITOR_H_
 
